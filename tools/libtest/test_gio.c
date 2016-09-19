61b6bd01940eb5e7d97ea967c126a9d641339a96
Sangjung Woo <again4you@gmail.com>
2016-09-19 05:43:31 -0700

    Add test code
    Signed-off-by: Sangjung Woo <again4you@gmail.com>

--- tools/libtest/test_gio.c
+++ tools/libtest/test_gio.c
@@ -71,6 +71,7 @@ static void get_property_all(GDBusProxy *proxy, const char *name)
 }
 
 // TODO need to reture value
+// https://github.com/kees-jan/DBus-GDK/blob/master/src/DBusBinding-gio.cc
 static void get_property_one(GDBusProxy *proxy, const char *obj_name, const char *property_name)
 {
 	GError *err;
@@ -94,10 +95,41 @@ static void get_property_one(GDBusProxy *proxy, const char *obj_name, const char
 	s = g_variant_print(ret, TRUE);
 	printf("%s\n", s);
 
+
+
 	g_free(s);
 	g_variant_unref(ret);
 	return ;
 }
+
+static void set_property_one(GDBusProxy *proxy, const char *obj_name, const char *property_name, GVariant *value)
+{
+	GError *err;
+	GVariant *ret;
+	gchar *s;
+
+	err = NULL;
+	ret = g_dbus_proxy_call_sync(proxy, 
+				"Set",
+				g_variant_new("(ssv)", obj_name, property_name, value),
+				G_DBUS_PROXY_FLAGS_NONE,
+				-1,
+				NULL,
+				&err);
+	if (!ret) {
+		fprintf(stderr, "Error(%s): %s\n", __func__, err->message);
+		g_clear_error(&err);
+		return ;
+	}
+
+	s = g_variant_print(ret, TRUE);
+	printf("%s\n", s);
+
+	g_free(s);
+	g_variant_unref(ret);
+	return ;
+}
+
 
 /************************************************************************/
 /* private */
@@ -152,8 +184,9 @@ static GList *find_objects(GDBusProxy *proxy, const char *name)
 	GError *err;
 	GVariant *ret;
 	GVariantIter *iter;
-	GList *list;
+	GList *list = NULL;
 	const gchar *obj;
+    gchar *s;
 
 	err = NULL;
 	ret = g_dbus_proxy_call_sync(proxy,
@@ -169,6 +202,9 @@ static GList *find_objects(GDBusProxy *proxy, const char *name)
 		return NULL;
 	}
 
+    s = g_variant_print(ret, TRUE);
+    printf("%s\n", s);
+
 	g_variant_get(ret, "(ao)", &iter);
 	while (g_variant_iter_loop(iter, "o", &obj)) {
 		GDBusProxy *objproxy;
@@ -180,10 +216,47 @@ static GList *find_objects(GDBusProxy *proxy, const char *name)
 
 	g_variant_iter_free(iter);
 	g_variant_unref(ret);
+    g_free(s);
 
 	return list;
 }
 
+static GList *find_objects_for_zone(GDBusProxy *proxy, const char *name, int zone)
+{
+	GError *err;
+	GVariant *ret;
+	GList *list = NULL;
+    gchar *s;
+	const gchar *obj;
+	GDBusProxy *objproxy;
+
+	err = NULL;
+	ret = g_dbus_proxy_call_sync(proxy,
+				"FindObjectForZone",
+				g_variant_new("(si)", name, zone),
+				G_DBUS_PROXY_FLAGS_NONE,
+				-1,
+				NULL,
+				&err);
+	if (!ret) {
+		fprintf(stderr, "Error(%s): %s\n", __func__, err->message);
+		g_clear_error(&err);
+		return NULL;
+	}
+
+    s = g_variant_print(ret, TRUE);
+    printf("%s\n", s);
+
+	g_variant_get(ret, "(o)", &obj);
+	objproxy = get_proxy_from_obj(obj);
+	if (objproxy)
+		list = g_list_append(list, objproxy);
+
+	g_variant_unref(ret);
+    g_free(s);
+
+	return list;
+}
 /************************************************************************/
 
 int main()
@@ -209,7 +282,15 @@ int main()
 		get_property_one(l->data, "org.automotive.VehicleSpeed", "Speed");
 	}
 
-	
+    // 3. set property
+	objlist = find_objects_for_zone(mgr, "ClimateControl", 5);
+	if (!objlist)
+		return 1;
+
+	for (l = objlist; l != NULL; l = l->next) {
+		get_property_all(l->data, "org.automotive.ClimateControl");
+        set_property_one(l->data, "org.automotive.ClimateControl", "AirConditioning", g_variant_new("b", TRUE));
+	}
 
 	g_object_unref(mgr);
 	return 0;

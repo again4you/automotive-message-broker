594329b029a736e648686cd006b8394eb25f7730
Sangjung Woo <sangjung.woo@samsung.com>
2016-09-26 11:49:28 +0900

    sj_work: client lib work - 1
    - use DebugOut function instead of stdout & stderr.
    - fix warning & error message.
    
    Signed-off-by: Sangjung Woo <sangjung.woo@samsung.com>

--- lib/client/libamb-client.cpp
+++ lib/client/libamb-client.cpp
@@ -22,6 +22,7 @@
 #include <errno.h>
 
 #include "libamb-client.h"
+#include "debugout.h"
 
 #ifndef EXPORT
 #  define EXPORT __attribute__((visibility("default")))
@@ -51,7 +52,7 @@ static GDBusProxy *get_manager()
 			NULL,
 			&err);
 	if (!proxy) {
-		fprintf(stderr, "Error(%s): %s\n", __func__, err->message);
+		DebugOut(DebugOut::Error) << __func__ << ": " << err->message;
 		g_clear_error(&err);
 		return NULL;
 	}
@@ -73,7 +74,7 @@ static GDBusProxy *get_proxy_from_obj(const char *objpath)
 			NULL,
 			&err);
 	if (!proxy) {
-		fprintf(stderr, "Error: %s(): %s\n", __func__, err->message);
+		DebugOut(DebugOut::Error) << __func__ << ": " << err->message;
 		g_clear_error(&err);
 		return NULL;
 	}
@@ -92,12 +93,12 @@ static GDBusProxy *find_objects_with_zone(GDBusProxy *proxy, const char *obj_nam
 	ret = g_dbus_proxy_call_sync(proxy,
 				"FindObjectForZone",
 				g_variant_new("(si)", obj_name, zone),
-				G_DBUS_PROXY_FLAGS_NONE,
+				G_DBUS_CALL_FLAGS_NONE,
 				-1,
 				NULL,
 				&err);
 	if (!ret) {
-		fprintf(stderr, "Error(%s): %s\n", __func__, err->message);
+		DebugOut(DebugOut::Error) << __func__ << ": " << err->message;
 		g_clear_error(&err);
 		return NULL;
 	}
@@ -122,12 +123,12 @@ static GList *find_objects(GDBusProxy *proxy, const char *obj_name)
 	ret = g_dbus_proxy_call_sync(proxy,
 			"FindObject",
 			g_variant_new("(s)", obj_name),
-			G_DBUS_PROXY_FLAGS_NONE,
+			G_DBUS_CALL_FLAGS_NONE,
 			-1,
 			NULL,
 			&err);
 	if (!ret) {
-		fprintf(stderr, "Error(%s): %s\n", __func__, err->message);
+		DebugOut(DebugOut::Error) << __func__ << ": " << err->message;
 		g_clear_error(&err);
 		return NULL;
 	}
@@ -161,12 +162,12 @@ static GVariant *get_all(GDBusProxy *proxy, const char *name)
 	ret = g_dbus_proxy_call_sync(proxy,
 			"GetAll",
 			g_variant_new("(s)", obj_name),
-			G_DBUS_PROXY_FLAGS_NONE,
+			G_DBUS_CALL_FLAGS_NONE,
 			-1,
 			NULL,
 			&err);
 	if (!ret) {
-		fprintf(stderr, "Error(%s): %s\n", __func__, err->message);
+		DebugOut(DebugOut::Error) << __func__ << ": " << err->message;
 		g_clear_error(&err);
 		return NULL;
 	}
@@ -189,12 +190,12 @@ static int set_prop(GDBusProxy *proxy, const char *name, const char *prop_name, 
 	ret = g_dbus_proxy_call_sync(proxy,
 				"Set",
 				g_variant_new("(ssv)", obj_name, prop_name, value),
-				G_DBUS_PROXY_FLAGS_NONE,
+				G_DBUS_CALL_FLAGS_NONE,
 				-1,
 				NULL,
 				&err);
 	if (!ret) {
-		fprintf(stderr, "Error(%s): %s\n", __func__, err->message);
+		DebugOut(DebugOut::Error) << __func__ << ": " << err->message;
 		g_clear_error(&err);
 		return -1;
 	}
@@ -240,7 +241,6 @@ EXPORT int get_property_all_with_zone(GVariant **proplist, const char *obj_name,
 {
 	GDBusProxy *proxy;
 	GDBusProxy *objproxy;
-	// GVariant *ret;
 
 	proxy = get_manager();
 	if (!proxy)
@@ -266,7 +266,6 @@ EXPORT int get_property_all(GList **proplist, const char *obj_name)
 	GVariant *ret;
 	GList *obj;
 	GList *objlist;
-	// GList *retlist;
 
 	proxy = get_manager();
 	if (!proxy)
@@ -278,7 +277,7 @@ EXPORT int get_property_all(GList **proplist, const char *obj_name)
 
 	*proplist = NULL;
 	for (obj = objlist; obj != NULL; obj = obj->next) {
-		ret = get_all(obj->data, obj_name);
+		ret = get_all(static_cast<GDBusProxy*>(obj->data), obj_name);
 		*proplist = g_list_append(*proplist, ret);
 	}
 
@@ -301,13 +300,13 @@ EXPORT int get_object_list(GList **objlist)
 	ret = g_dbus_proxy_call_sync(proxy,
 			"List",
 			NULL,
-			G_DBUS_PROXY_FLAGS_NONE,
+			G_DBUS_CALL_FLAGS_NONE,
 			-1,
 			NULL,
 			&err);
 
 	if (!ret) {
-		fprintf(stderr, "Error(%s): %s\n", __func__, err->message);
+		DebugOut(DebugOut::Error) << __func__ << ": " << err->message;
 		g_clear_error(&err);
 		return -1;
 	}

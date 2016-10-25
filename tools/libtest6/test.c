c1ac0a3e7eb7c80bb036e308e9f986fa227b6edb
Sangjung Woo <sangjung.woo@samsung.com>
2016-10-25 18:55:43 +0900

    sj_work: modify libclient v3
    Signed-off-by: Sangjung Woo <sangjung.woo@samsung.com>

--- tools/libtest6/test.c
+++ tools/libtest6/test.c
@@ -57,20 +57,26 @@ static void VehicleOdometer_handler(const gchar *objname, gpointer data, void *u
 }
 
 struct user_data_test *udata;
+struct user_data_test *udata2;
 
 static gboolean timer_callback(gpointer d)
 {
 	int rc;
 	gchar *objname = (gchar *)d;
 
-	rc = amb_unregister_property_changed_handler(objname, 0);
+#if 0
+	rc = amb_unregister_property_changed_handler(objname, 0, 1);
 	if (rc != 0) {
 		fprintf(stderr, "Fail to amb_unregister_property_changed_handler(): %s\n", objname);
 	}
+#endif
+
+#if 1
 	// cleanup
 	free(udata->name);
 	g_free(udata);
 	exit(0);
+#endif
 
 	return FALSE;
 }
@@ -78,6 +84,8 @@ static gboolean timer_callback(gpointer d)
 void test_VehicleOdometer_listen()
 {
 	GMainLoop *loop;
+	guint32 id1;
+	guint32 id2;
 	int rc;
 
 	udata = g_new0(struct user_data_test, 1);
@@ -87,11 +95,34 @@ void test_VehicleOdometer_listen()
 	rc = amb_register_property_changed_handler("VehicleOdometer",
 					0,
 					(AMB_PROPERTY_CHANGED_CALLBACK)VehicleOdometer_handler,
-					(void *)udata);
+					(void *)udata, &id1);
 	if (rc != 0) {
                 fprintf(stderr, "Fail to amb_register_property_changed_handler(): %s\n", "VehicleOdometer");
                 return ;
 	}
+
+#if 0
+	udata2 = g_new0(struct user_data_test, 1);
+	udata2->value = 100;
+	udata2->name = strdup("Test Name2");
+	rc = amb_register_property_changed_handler("VehicleOdometer",
+					0,
+					(AMB_PROPERTY_CHANGED_CALLBACK)VehicleOdometer_handler,
+					(void *)udata2, &id2);
+	if (rc != 0) {
+                fprintf(stderr, "Fail to amb_register_property_changed_handler(): %s\n", "VehicleOdometer");
+                return ;
+	}
+	fprintf(stderr, "id1: %u, id2: %u\n", id1, id2);
+#endif
+
+#if 1
+	rc = amb_unregister_property_changed_handler("VehicleOdometer", 0, id1);
+	if (rc != 0) {
+                fprintf(stderr, "Fail to amb_unregister_property_changed_handler(): %u\n", id1);
+		return ;
+	}
+#endif
 
 	g_timeout_add_seconds(10, timer_callback, "VehicleOdometer");
 	loop = g_main_loop_new(NULL, FALSE);
@@ -99,8 +130,101 @@ void test_VehicleOdometer_listen()
 
 	g_main_loop_unref(loop);
 }
+
+
+GList *cb_list;
+
+struct callback_item {
+	AMB_PROPERTY_CHANGED_CALLBACK callback;
+	void *user_data;
+	gboolean is_delete;
+	guint32 cid;
+};
+
+guint32 cid;
+
 int main()
 {
+#if 1
 	test_VehicleOdometer_listen();
+#endif
+	
+#if 0
+	struct callback_item *citem01;
+	struct callback_item *citem02;
+	GList *l;
+	GList *ll;
+	cid = 0;
+
+	citem01 = g_new0(struct callback_item, 1);
+	citem01->is_delete = FALSE;
+	citem01->user_data = (void *)strdup("item_01");
+	citem01->cid = __atomic_fetch_add(&cid, 1, __ATOMIC_RELAXED);
+
+	cb_list = g_list_append(cb_list, citem01);
+
+	citem02 = g_new0(struct callback_item, 1);
+	citem02->is_delete = FALSE;
+	citem02->user_data = (void *)strdup("item_02");
+	citem02->cid = __atomic_fetch_add(&cid, 1, __ATOMIC_RELAXED);
+
+	cb_list = g_list_append(cb_list, citem02);
+
+#if 0
+	ncid = __atomic_fetch_add(&cid, 1, __ATOMIC_RELAXED);
+	fprintf(stderr, "cid: %u, ncid: %u\n", cid, ncid);
+	ncid = __atomic_fetch_add(&cid, 1, __ATOMIC_RELAXED);
+	fprintf(stderr, "cid: %u, ncid: %u\n", cid, ncid);
+#endif
+
+	// list
+	fprintf(stderr, "\n* Listing\n");
+	for (l = cb_list; l != NULL; l = l->next) {
+		struct callback_item *citem = l->data;
+		fprintf(stderr, "citem->cid: %u\n", citem->cid);	
+		fprintf(stderr, "citem->user_data: %s\n", (char*)citem->user_data);	
+	}
+
+
+	// find 
+	fprintf(stderr, "\n* Finding\n");
+	for (l = cb_list; l != NULL; l = l->next) {
+		struct callback_item *citem = l->data;
+		citem->is_delete = TRUE;
+		fprintf(stderr, "citem->cid: %u\n", citem->cid);	
+		fprintf(stderr, "citem->user_data: %s\n", (char*)citem->user_data);	
+	}
+	
+	// delete
+	fprintf(stderr, "\n* Deleting\n");
+	for (l = cb_list, ll = g_list_next(l);
+		l != NULL;
+		l = ll, ll = g_list_next(ll)) {
+		struct callback_item *citem = l->data;
+		fprintf(stderr, "citem->cid: %u\n", citem->cid);	
+		if (citem->is_delete) {
+			fprintf(stderr, "Delete citem->cid: %u\n", citem->cid);	
+			cb_list = g_list_delete_link(cb_list, l);
+		}
+
+	}
+
+	// list
+	fprintf(stderr, "\n* Listing\n");
+	for (l = cb_list; l != NULL; l = l->next) {
+		struct callback_item *citem = l->data;
+		fprintf(stderr, "citem->cid: %u\n", citem->cid);	
+		fprintf(stderr, "citem->user_data: %s\n", (char*)citem->user_data);	
+	}
+
+	if (g_list_length(cb_list) == 0) {
+		fprintf(stderr, "cb_list item is 0\n");
+	}
+
+	// cleanup
+	g_free(citem01);
+	g_free(citem02);
+#endif
+
 	return 0;
 }

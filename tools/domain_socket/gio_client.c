aef3b68cedeede85ffa432d5addffa9b150d77fd
Sangjung Woo <again4you@gmail.com>
2017-07-19 19:36:19 +0900

    Add test code
    Signed-off-by: Sangjung Woo <again4you@gmail.com>

--- tools/domain_socket/gio_client.c
+++ tools/domain_socket/gio_client.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 #include <glib.h>
 #include <gio/gio.h>
@@ -27,25 +28,54 @@ typedef struct {
 	guint32 buf_size;
 } msg_buf_t;
 
-int main()
+int
+timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y)
+{
+    if (x->tv_usec < y->tv_usec) {
+        int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
+        y->tv_usec -= 1000000 * nsec;
+        y->tv_sec += nsec;
+    }
+    if (x->tv_usec - y->tv_usec > 1000000) {
+        int nsec = (x->tv_usec - y->tv_usec) / 1000000;
+        y->tv_usec += 1000000 * nsec;
+        y->tv_sec -= nsec;
+    }
+
+    result->tv_sec = x->tv_sec - y->tv_sec;
+    result->tv_usec = x->tv_usec - y->tv_usec;
+
+    return x->tv_sec < y->tv_sec;
+}
+
+int main(int argc, char **argv)
 {
 	GError *error;
 	GSocket *socket;
 	GSocketAddress *sock_addr;
 
+    struct timeval start_time, end_time, diff_time;
+
 	msg_req_t msg_rev;
-
-	msg_req_t *msg_req;
+	msg_req_t msg_req;
 	msg_hdr_t *msg_hdr;
-	msg_req = malloc(sizeof(*msg_req));
-	memset(msg_req, 0x00, sizeof(*msg_req));
+	memset(&msg_req, 0x00, sizeof(msg_req_t));
 
-	msg_hdr = (msg_hdr_t *)msg_req;
-	msg_hdr->mode = 0;
-	msg_hdr->object = 101;
-	msg_hdr->size = sizeof(*msg_req);
-	msg_req->value = 200;
+    if (argc < 3) {
+        g_printerr("%s: [OP] [OBJECT_NUM]\n", argv[0]);
+        return 0;
+    }
 
+	msg_hdr = (msg_hdr_t *)&msg_req;
+	msg_hdr->mode = atoi(argv[1]);
+    msg_hdr->object = atoi(argv[2]);
+	msg_hdr->size = sizeof(msg_req_t);
+    if (msg_hdr->mode == 1)
+        msg_req.value = atoi(argv[3]);
+
+    // START TIME MEASURE
+    gettimeofday(&start_time, NULL);
+    
 	// socket create
 	error = NULL;
 	socket = g_socket_new(G_SOCKET_FAMILY_UNIX,
@@ -71,7 +101,7 @@ int main()
 	}
 
 	// request
-	g_socket_send(socket, (const gchar*)msg_req, sizeof(msg_req_t), NULL, &error);
+	g_socket_send(socket, (const gchar*)&msg_req, sizeof(msg_req_t), NULL, &error);
 	if (error != NULL) {
 		g_printerr("g_socket_send Error: %s\n", error->message);
 		g_error_free(error);
@@ -86,8 +116,8 @@ int main()
 		return -1;
 	}
 
-	g_printerr("msg_head.mode: %d\n", msg_rev.msg_head.mode);
-	g_printerr("Odometer value: %d\n", msg_rev.value);
+    // END TIME MEASURE
+    gettimeofday(&end_time, NULL);
 
 	g_socket_close(socket, &error);
 	if (error) {
@@ -95,6 +125,11 @@ int main()
 		g_error_free(error);
 		return -1;
 	}
+
+    timeval_subtract(&diff_time, &end_time, &start_time);
+	g_printerr("msg_head.mode: %d\n", msg_rev.msg_head.mode);
+	g_printerr("Odometer value: %d\n", msg_rev.value);
+    g_printerr("%ld.%06ld sec\n", diff_time.tv_sec, diff_time.tv_usec);
 
 	return 0;
 }

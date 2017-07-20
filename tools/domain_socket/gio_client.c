9a991d6ec6b77e308c933017add03fb8c1263563
Sangjung Woo <sangjung.woo@samsung.com>
2017-07-20 19:16:54 +0900

    modified test program
    Signed-off-by: Sangjung Woo <sangjung.woo@samsung.com>

--- tools/domain_socket/gio_client.c
+++ tools/domain_socket/gio_client.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
@@ -8,128 +9,235 @@
 #include <gio/gio.h>
 #include <gio/gunixsocketaddress.h>
 
-#define SOCK_PATH "/tmp/socket_file"
+#define SOCK_PATH "/run/amb_socket"
 #define BUFSIZE 1024
 
-typedef struct {
-	unsigned int mode;
-	unsigned int object;
-	unsigned int size;
-} msg_hdr_t;
+struct udsmsg_prop {
+	int32_t zone;
+	uint32_t len;
+	uint8_t name[0];
+};
 
-typedef struct {
-	msg_hdr_t msg_head;
-	int value;
-} msg_req_t;
+struct udsmsg_val {
+	int32_t res;
+	uint16_t type; /* gvariant type char */
+	uint16_t len;
+	uint8_t val[0];
+};
 
-typedef struct {
-	guint8	*data;
-	guint32	data_size;
-	guint32 buf_size;
-} msg_buf_t;
+enum udsmsg_type {
+	UT_GET_REQ,
+	UT_GET_RES,
+	UT_SET_REQ,
+	UT_SET_RES,
+	UT_END,
+};
 
-int
-timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y)
+struct udsmsg_head {
+	uint32_t type; /* enum UDSMSG_TYPE */
+	uint32_t len;
+};
+
+static int udsmsg_fill_get_req(uint8_t *buf, uint32_t size,
+		int zone, const char *name)
 {
-    if (x->tv_usec < y->tv_usec) {
-        int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-        y->tv_usec -= 1000000 * nsec;
-        y->tv_sec += nsec;
-    }
-    if (x->tv_usec - y->tv_usec > 1000000) {
-        int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-        y->tv_usec += 1000000 * nsec;
-        y->tv_sec -= nsec;
-    }
+	int len;
+	struct udsmsg_head *uh;
+	struct udsmsg_prop *up;
 
-    result->tv_sec = x->tv_sec - y->tv_sec;
-    result->tv_usec = x->tv_usec - y->tv_usec;
+	if (!buf || size == 0 || !name)
+		return -1;
 
-    return x->tv_sec < y->tv_sec;
+	len = strlen(name) + 1;
+	if (len + sizeof(*uh) + sizeof(*up) > size)
+		return -1;
+
+	up = (void *)buf + sizeof(*uh);
+	up->zone = zone;
+	up->len = len;
+	memcpy(up->name, name, len);
+
+	uh = (void *)buf;
+	uh->type = UT_GET_REQ;
+	uh->len = sizeof(*uh) + sizeof(*up) + len;
+
+	return uh->len;
 }
 
-int main(int argc, char **argv)
+static int udsmsg_fill_set_req(uint8_t *buf, uint32_t size,
+		int zone, const char *name,
+		uint8_t vtype, uint16_t vsz, void *value)
 {
-	GError *error;
-	GSocket *socket;
-	GSocketAddress *sock_addr;
+	int len;
+	struct udsmsg_head *uh;
+	struct udsmsg_prop *up;
+	struct udsmsg_val *uv;
 
-    struct timeval start_time, end_time, diff_time;
+	if (!buf || size == 0 || !name || !value)
+		return -1;
 
-	msg_req_t msg_rev;
-	msg_req_t msg_req;
-	msg_hdr_t *msg_hdr;
-	memset(&msg_req, 0x00, sizeof(msg_req_t));
+	len = strlen(name) + 1;
+	if (len + vsz + sizeof(*uh) + sizeof(*up) + sizeof(*uv) > size)
+		return -1;
+
+	uv = (void *)buf + sizeof(*uh) + sizeof(*up) + len;
+	uv->res = 0;
+	uv->type = vtype;
+	uv->len = vsz;
+	memcpy(uv->val, value, vsz);
+
+	up = (void *)buf + sizeof(*uh);
+	up->zone = zone;
+	up->len = len;
+	memcpy(up->name, name, len);
+
+	uh = (void *)buf;
+	uh->type = UT_SET_REQ;
+	uh->len = sizeof(*uh) + sizeof(*up) + len + sizeof(*uv) + vsz;
+
+	return uh->len;
+}
+
+int main(int argc, char *argv[])
+{
+    GError *error;
+    GSocket *socket;
+    GSocketAddress *sock_addr;
+
+    gssize nread;
+    char buf[BUFSIZE] = {0, };
+    char bufread[BUFSIZE] = {0, };
+
+    int zone;
 
     if (argc < 3) {
-        g_printerr("%s: [OP] [OBJECT_NUM]\n", argv[0]);
-        return 0;
+        printf(" usage) %s {set|get} zone name vtype value\n", argv[0]);
+        printf("   zone:\n");
+        printf("     None = 0,\n");
+        printf("     Front = 1,\n");
+        printf("     Middle = 2,\n");
+        printf("     Right = 4,\n");
+        printf("     Left = 8,\n");
+        printf("     Rear = 16,\n");
+        printf("     Center = 32,\n");
+        printf("     LeftSide = 64,\n");
+        printf("     RightSide = 128,\n");
+        printf("     FrontSide = 256,\n");
+        printf("     BackSide = 512\n");
+	printf("   vtype - i, s, f\n");
+	return 1;
     }
 
-	msg_hdr = (msg_hdr_t *)&msg_req;
-	msg_hdr->mode = atoi(argv[1]);
-    msg_hdr->object = atoi(argv[2]);
-	msg_hdr->size = sizeof(msg_req_t);
-    if (msg_hdr->mode == 1)
-        msg_req.value = atoi(argv[3]);
+    error = NULL;
+    socket = g_socket_new(G_SOCKET_FAMILY_UNIX,
+                        G_SOCKET_TYPE_STREAM,
+                        G_SOCKET_PROTOCOL_DEFAULT,
+                        &error);
+    if (error) {
+        g_printerr("Fail to g_socket_new: %s\n", error->message);
+        g_error_free(error);
+        return -1;
+    }
 
-    // START TIME MEASURE
-    gettimeofday(&start_time, NULL);
-    
-	// socket create
-	error = NULL;
-	socket = g_socket_new(G_SOCKET_FAMILY_UNIX,
-						G_SOCKET_TYPE_STREAM,
-						G_SOCKET_PROTOCOL_DEFAULT,
-						&error);
-	if (error != NULL) {
-		g_printerr("g_socket_new Error: %s\n", error->message);
-		g_error_free(error);
-		return -1;
+    sock_addr = g_unix_socket_address_new_with_type(SOCK_PATH,
+                                        -1, G_UNIX_SOCKET_ADDRESS_PATH);
+    if (!g_socket_connect(socket, sock_addr, NULL, &error)) {
+        g_printerr("Fail to g_socket_connect: %s\n", error->message);
+        g_error_free(error);
+        return -1;
+    }
+
+	zone = atoi(argv[2]);
+	if (!strcmp(argv[1], "set")) {
+		void *v;
+		int vsz;
+		int v_i;
+		double v_f;
+
+		switch(argv[4][0]) {
+			case 's':
+				v = argv[5];
+				vsz = strlen(v);
+				break;
+			case 'i':
+				v_i = atoi(argv[5]);
+				v = &v_i;
+				vsz = sizeof(v_i);
+				break;
+			case 'f':
+				v_f = atof(argv[5]);
+				v = &v_f;
+				vsz = sizeof(v_f);
+				break;
+			default:
+				g_printerr("Unknown value type: '%c'\n", argv[4][0]);
+				return 1;
+		}
+
+		nread = udsmsg_fill_set_req((uint8_t *)buf, sizeof(buf), zone, argv[3],
+				argv[4][0], vsz, v);
+	} else if (!strcmp(argv[1], "get")) {
+		nread = udsmsg_fill_get_req((uint8_t *)buf, sizeof(buf), zone, argv[3]);
+	} else {
+		g_printerr("Unknown command: %s\n", argv[1]);
+		return 1;
 	}
 
-	// server address setting
-	sock_addr = g_unix_socket_address_new_with_type(SOCK_PATH,
-												-1,
-												G_UNIX_SOCKET_ADDRESS_PATH);
+    g_socket_send(socket, buf, nread, NULL, &error);
+    if (error) {
+        g_printerr("Fail to g_socket_send: %s\n", error->message);
+        g_error_free(error);
+        return -1;
+    }
 
-	// connect
-	if (g_socket_connect(socket, sock_addr, NULL, &error) == FALSE) {
-		g_printerr("g_socket_connect Error: %s\n", error->message);
-		g_error_free(error);
-		return -1;
+	{
+		struct udsmsg_head uh;
+		struct udsmsg_prop *up;
+		struct udsmsg_val *uv;
+
+		nread = g_socket_receive(socket, (gchar *)&uh, sizeof(uh), NULL, &error);
+		if (error) {
+			g_printerr("Fail to g_socket_receive: %s\n", error->message);
+			g_error_free(error);
+			return -1;
+		}
+		printf("Recv: [%s] %d\n", uh.type == UT_SET_RES ? "Set" : "Get", uh.len);
+
+		nread = g_socket_receive(socket, bufread, uh.len - sizeof(uh), NULL, &error);
+		if (error) {
+			g_printerr("Fail to g_socket_receive: %s\n", error->message);
+			g_error_free(error);
+			return -1;
+		}
+
+		up = (void *)bufread;
+		printf(" zone %d len %u [%s]\n", up->zone, up->len, up->name);
+
+		uv = (void *)bufread + sizeof(*up) + up->len;
+		printf(" type '%c' len %u", uv->type, uv->len);
+		switch (uv->type) {
+			case 's':
+				printf(" [%s]\n", uv->val);
+				break;
+			case 'i':
+				printf(" [%d]\n", *(int *)uv->val);
+				break;
+			case 'f':
+				printf(" [%lf]\n", *(double *)uv->val);
+				break;
+			default:
+				printf(" Unknown\n");
+				break;
+		}
 	}
-
-	// request
-	g_socket_send(socket, (const gchar*)&msg_req, sizeof(msg_req_t), NULL, &error);
-	if (error != NULL) {
-		g_printerr("g_socket_send Error: %s\n", error->message);
-		g_error_free(error);
-		return -1;
-	}
-
-	// get result
-	g_socket_receive(socket, (gchar*)&msg_rev, sizeof(msg_req_t), NULL, &error);
-	if (error) {
-		g_printerr("g_socket_receive Error: %s\n", error->message);
-		g_error_free(error);
-		return -1;
-	}
-
-    // END TIME MEASURE
-    gettimeofday(&end_time, NULL);
 
 	g_socket_close(socket, &error);
 	if (error) {
-		g_printerr("g_socket_close Error: %s\n", error->message);
-		g_error_free(error);
-		return -1;
-	}
+        g_printerr("Fail to g_socket_close: %s\n", error->message);
+        g_error_free(error);
+        return -1;
+    }
+    g_object_unref(sock_addr);
 
-    timeval_subtract(&diff_time, &end_time, &start_time);
-	g_printerr("msg_head.mode: %d\n", msg_rev.msg_head.mode);
-	g_printerr("Odometer value: %d\n", msg_rev.value);
-    g_printerr("%ld.%06ld sec\n", diff_time.tv_sec, diff_time.tv_usec);
-
-	return 0;
+    return 0;
 }
